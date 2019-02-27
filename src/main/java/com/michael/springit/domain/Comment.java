a58b5274b9269bfaa4b00b32914dae6107531ae1
mickielreid <18forcash@gmail.com>
2019-02-26 23:13:31 -0500

    Create our domainentities and refactor using project lombok

---
+++ src/main/java/com/michael/springit/domain/Comment.java
@@ -0,0 +1,23 @@
+package com.michael.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String body;
+
+
+    //link
+
+}
